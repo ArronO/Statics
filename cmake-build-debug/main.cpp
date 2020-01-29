0386813b108d42af6a43f0fce9ef5142389ac76a
jameys127 <jeremiah_sheehy1@my.vcccd.edu>
2020-01-28 16:43:35 -0800

    :/

--- cmake-build-debug/main.cpp
+++ cmake-build-debug/main.cpp
@@ -8,6 +8,7 @@ double mag(double x, double y, double z)
 
 double firstForce[] = {00, 01, 02, 03}; //magnitude, x, y, z
 double secondForce[] = {10, 11, 12, 13}; //magnitude, x, y, z
+double thirdForce[] = {20, 21, 22, 23}; //magnitude, x, y, z
 
 double uFirstForce[] = {00, 01, 02}; //x, y, z
 double uSecondForce[] = {10, 11, 12}; //x, y, z
@@ -15,6 +16,7 @@ double uThirdForce[] = {20, 21, 22}; //x, y, z
 
 double firstForcemag[] = {00, 01, 02};
 double secondForcemag[] = {10, 11, 12};
+double thirdForcemag[] = {20, 21, 22};
 
 double ufirstForcemag[] = {00, 01, 02};
 double usecondForcemag[] = {10, 11, 12};
@@ -26,6 +28,8 @@ double placeholder[] = {0, 1, 2};
 
 double knownForceValue[] = {0, 0, 0};
 
+int numberOfForces = 0;
+
 double det = 0;
 double detx = 0;
 double dety = 0;
@@ -35,16 +39,19 @@ double I = 0;
 double II = 0;
 double III = 0;
 
-double *knownCO[2] = {firstForce, secondForce};
+double *knownCO[3] = {firstForce, secondForce, thirdForce};
 double *unknownCO[3] = {uFirstForce, uSecondForce, uThirdForce};
 
-double *knownCOmag[2] = {firstForcemag, secondForcemag};
+double *knownCOmag[3] = {firstForcemag, secondForcemag, thirdForcemag};
 double *unknownCOmag[5] = {ufirstForcemag, usecondForcemag, uthirdForcemag, ufirstForcemag2, usecondForcemag2};
 
 char cinco[] = {'x', 'y', 'z'};
 
 int main()
 {
+    std::cout << "Please enter the number of known forces, from 1-3\nNumber: ";
+    std::cin >> numberOfForces;
+
     std::cout << "Please enter the magnitude and x, y, and z directional components of the first known force\nMagnitude: ";
     for (int i = 0; i < 4; i++)
     {
@@ -56,15 +63,32 @@ int main()
         std::cin >> knownCO[0][i];
     }
 
-    std::cout << "Please enter the magnitude and x, y, and z directional components of the second known force\nMagnitude: ";
-    for (int i = 0; i < 4; i++)
+    if(numberOfForces > 1)
     {
-        if(i != 0)
+        std::cout << "Please enter the magnitude and x, y, and z directional components of the second known force\nMagnitude: ";
+        for (int i = 0; i < 4; i++)
         {
-            std::cout << cinco[i-1] << ": ";
-        }
+            if(i != 0)
+            {
+                std::cout << cinco[i-1] << ": ";
+            }
 
-        std::cin >> knownCO[1][i];
+            std::cin >> knownCO[1][i];
+        }
+    }
+
+    if(numberOfForces > 2)
+    {
+        std::cout << "Please enter the magnitude and x, y, and z directional components of the third known force\nMagnitude: ";
+        for (int i = 0; i < 4; i++)
+        {
+            if(i != 0)
+            {
+                std::cout << cinco[i-1] << ": ";
+            }
+
+            std::cin >> knownCO[2][i];
+        }
     }
 
     std::cout << "Please enter the x, y, and z directional components of the first unknown force" << std::endl;
@@ -96,6 +120,10 @@ int main()
         {
             knownCOmag[1][i - 1] = (knownCO[1][0] * knownCO[1][i]) / mag(knownCO[1][1], knownCO[1][2], knownCO[1][3]);
         } //second force direction
+        for (int i = 1; i < 4; i++)
+        {
+            knownCOmag[2][i - 1] = (knownCO[2][0] * knownCO[2][i]) / mag(knownCO[2][1], knownCO[2][2], knownCO[2][3]);
+        } //third force direction
         for (int i = 0; i < 3; i++)
         {
             unknownCOmag[0][i] = unknownCO[0][i] / mag(unknownCO[0][0], unknownCO[0][1], unknownCO[0][2]);
@@ -114,9 +142,20 @@ int main()
             unknownCOmag[4][i] = unknownCOmag[1][i];
         } //determinant setup
         for (int i = 0; i < 3; i++)
-    {
-        knownForceValue[i] = knownCOmag[0][i] + knownCOmag[1][i];
-    } //answer column setup
+        {
+            if (numberOfForces = 1)
+            {
+                knownForceValue[i] = knownCOmag[0][i];
+            }
+            if (numberOfForces = 2)
+            {
+                knownForceValue[i] = knownCOmag[0][i] + knownCOmag[1][i];
+            }
+            if (numberOfForces = 3)
+            {
+                knownForceValue[i] = knownCOmag[0][i] + knownCOmag[1][i] + knownCOmag[2][i];
+            }
+        } //answer column setup
 
         //all of the directional vectors should be figured out at this point
 
@@ -163,18 +202,18 @@ int main()
 
     for (int i = 1; i < 3; i++)
     {
-        dety = dety + (unknownCOmag[i][0] * unknownCOmag[i+1][1] * unknownCOmag[i+2][2]);
-        dety = dety - (unknownCOmag[i+2][0] * unknownCOmag[i+1][1] * unknownCOmag[i][2]);
+        detz = detz + (unknownCOmag[i][0] * unknownCOmag[i+1][1] * unknownCOmag[i+2][2]);
+        detz = detz - (unknownCOmag[i+2][0] * unknownCOmag[i+1][1] * unknownCOmag[i][2]);
     } //evaluates detz
 
     I = detx / det;
     II = dety / det;
     III = detz / det;
 
-    std::cout << det << std::endl;
-    std::cout << knownForceValue[0] << std::endl;
+    //std::cout << det << std::endl;
+    //std::cout << knownForceValue[0] << std::endl;
 
-    //std::cout << "Your force values are:\n#1: " << I << "\n#2: " << II << "\n#3: " << III << std::endl;
+    std::cout << "Your force values are:\n#1: " << I << "\n#2: " << II << "\n#3: " << III << std::endl;
 
     return EXIT_SUCCESS;
 }
